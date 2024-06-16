317be6917330740d5f461e33f747b542a142c616
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2024-06-15 23:19:55 -0400

    big select query better null handling + formatting

--- sql/select.sql
+++ sql/select.sql
@@ -1,14 +1,28 @@
-with
-goodjobs as (
-    select j.jid, oaCount, int1Count, int2Count, int3Count, offerCount from jobs j
-    left join (select jid, count(*) as oaCount from Contributions where oa='t' group by jid) oa on j.jid = oa.jid
-    left join (select jid, count(*) as int1Count from Contributions where InterviewStage>=1 group by jid) int1 on j.jid = int1.jid
-    left join (select jid, count(*) as int2Count from Contributions where InterviewStage>=2 group by jid) int2 on j.jid = int2.jid
-    left join (select jid, count(*) as int3Count from Contributions where InterviewStage>=3 group by jid) int3 on j.jid = int3.jid
-    left join (select jid, count(*) as offerCount from Contributions where offercall='t' group by jid) offer on j.jid = offer.jid
+with goodjobs as (
+    SELECT
+        JID,
+        SUM(CASE WHEN OA = 't' THEN 1 ELSE 0 END) oaCount,
+        SUM(CASE WHEN InterviewStage >= 1 THEN 1 ELSE 0 END) int1Count,
+        SUM(CASE WHEN InterviewStage >= 2 THEN 1 ELSE 0 END) int2Count,
+        SUM(CASE WHEN InterviewStage >= 3 THEN 1 ELSE 0 END) int3Count,
+        SUM(CASE WHEN OfferCall = 't' THEN 1 ELSE 0 END) offerCount
+    FROM Contributions
+    GROUP BY JID
 ),
 watches as (
-    select *, TRUE as watch from Watching where UID = 'j12cole'
+    SELECT *, TRUE as Watch from Watching where UID = 'j12cole'
 )
-select j.*, g.oacount, g.int1count, g.int2count, g.int3count, g.offercount, coalesce(w.watch, false) watched
-from jobs j left join goodjobs g on j.jid = g.jid left join watches w on j.jid = w.jid order by j.jid
+SELECT 
+    j.*,
+    COALESCE(g.oaCount, 0) oaCount,
+    COALESCE(g.int1Count, 0) int1Count,
+    COALESCE(g.int2Count, 0) int2Count,
+    COALESCE(g.int3Count, 0) int3Count,
+    COALESCE(g.OfferCount, 0) OfferCount,
+    COALESCE(w.Watch, FALSE) watched
+FROM jobs j
+LEFT JOIN goodjobs g 
+    ON j.JID = g.JID
+LEFT JOIN watches w 
+    ON j.JID = w.JID
+ORDER BY j.JID
