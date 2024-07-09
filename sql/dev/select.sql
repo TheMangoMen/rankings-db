ae4495553ae0a180fae5fcd559f5dcf70bb60640
Norman Chen <njchen@uwaterloo.ca>
2024-07-08 22:59:48 -0400

    update job select

--- sql/dev/select.sql
+++ sql/dev/select.sql
@@ -10,7 +10,7 @@ WITH interviewcounts AS (
     GROUP BY JID
 ),
 watches AS (
-    SELECT *, TRUE as watch from Watching where UID = $1
+    SELECT *, TRUE as watch from Watching where UID = 'njchen'
 )
 SELECT
     j.jid,
@@ -32,7 +32,8 @@ LEFT JOIN interviewcounts i
     ON j.JID = i.JID
 LEFT JOIN watches w
     ON j.JID = w.JID
-ORDER BY j.JID;
+WHERE j.season = (SELECT * FROM season) AND j.year = (SELECT * FROM year) AND j.cycle = (SELECT * FROM cycle)
+ORDER BY j.company;
 
 WITH rankingcounts AS (
     SELECT
@@ -44,7 +45,7 @@ WITH rankingcounts AS (
     GROUP BY JID
 ),
 watches AS (
-    SELECT *, TRUE AS watch FROM Watching WHERE UID = $1
+    SELECT *, TRUE AS watch FROM Watching WHERE UID = 'njchen'
 )
 SELECT
     j.jid,
@@ -58,11 +59,12 @@ SELECT
     COALESCE(r.ranked, 0) ranked,
     COALESCE(r.taking, 0) taking,
     COALESCE(r.nottaking, 0) nottaking,
-    COALESCE(w.Watch, FALSE) watched
+    COALESCE(w.Watch, FALSE) watching
 FROM jobs j
 LEFT JOIN rankingcounts r
     ON j.JID = r.JID
-LEFT JOIN watches w 
+LEFT JOIN watches w
     ON j.JID = w.JID
-ORDER BY j.JID;
+WHERE j.season = (SELECT * FROM season) AND j.year = (SELECT * FROM year) AND j.cycle = (SELECT * FROM cycle)
+ORDER BY j.company;
 
